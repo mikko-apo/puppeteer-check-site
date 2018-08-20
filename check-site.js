436c396bf4914661b940a0ed89979778a7ef782b
Mikko Apo <mikko.apo@reaktor.fi>
2018-08-20 10:51:46 +0300

    Crawled page is scrolled to the end, supports excluding of urls, added more http status codes, get error summary, updated puppeteer

--- check-site.js
+++ check-site.js
@@ -1,116 +1,247 @@
 const puppeteer = require('puppeteer');
 
-async function crawlUrl(page, url) {
-    const succeeded = [];
-    const failed = [];
+function debug(...args) {
+//  info(...args)
+}
 
-    function failedListener(request) {
-        console.log("failed request", request.url());
-        failed.push({
-            url: request.url(),
-            errorText: request.failure().errorText
-        });
+function info(...args) {
+  console.log(...args);
+}
+
+async function scrollToEnd(page) {
+  let scroll = true;
+  let originalPosition = await page.evaluate('window.pageYOffset');
+  while (scroll) {
+    await page.evaluate('window.scrollBy(0, window.innerHeight)');
+    let currentPosition = await page.evaluate('window.pageYOffset');
+    if (currentPosition != originalPosition) {
+      await page.waitFor(100);
+      originalPosition = currentPosition;
+    } else {
+      scroll = false
     }
+  }
+  return 1;
+}
 
-    function requestFinishedListener(request) {
-        console.log("request finished", request.url())
-    }
-
-    function responseListener(response) {
-        console.log("response", response.url());
-        if([200].includes(response.status())) {
-            succeeded.push(response.url());
+async function waitUntilEmpty(arr) {
+  return new Promise((resolve, reject) => {
+      const f = () => {
+        if (arr.length === 0) {
+          resolve();
         } else {
-            failed.push(response.url());
+          debug("Waiting for request to finish", arr)
+          setTimeout(f, 1000);
         }
+      }
+      setTimeout(f, 100);
     }
+  );
+}
 
-    page.on('requestfailed', failedListener);
-    page.on('response', responseListener);
+async function crawlUrl(page, crawlUrl) {
+  const succeeded = [];
+  const failed = [];
+  const openRequestUrls = [];
 
-    page.on('requestfinished', requestFinishedListener);
+  function isExcludedUrl(url) {
+    return url.includes('linkedin');
+  }
 
+  const requestListener = request => {
+    const url = request.url();
+    debug("request started", url)
+    if (isExcludedUrl(url)) {
+      request.abort();
+    } else {
+      openRequestUrls.push(url);
+      request.continue();
+    }
+  };
 
+  function requestFailedListener(request) {
+    const url = request.url();
+    debug("request failed", url)
+    removeFromArray(openRequestUrls, url);
+    if (!isExcludedUrl(url)) {
+      info("- failed", url, "errorText", request.failure().errorText)
+      failed.push({
+        url: url,
+        errorText: request.failure().errorText
+      });
+    }
+  }
+
+  function requestFinishedListener(request) {
+    const url = request.url();
+    debug("request finished", url)
+    removeFromArray(openRequestUrls, url);
+  }
+
+  function responseListener(response) {
+    debug("response", response.url())
+    if ([200, 204, 206, 301, 302, 304].includes(response.status())) {
+      succeeded.push(response.url());
+    } else {
+      info("- failed", response.url(), "status", response.status())
+      failed.push({url: response.url(), status: response.status()});
+    }
+  }
+
+  page.setRequestInterception(true);
+  page.on('request', requestListener);
+  page.on('requestfailed', requestFailedListener);
+  page.on('response', responseListener);
+  page.on('requestfinished', requestFinishedListener);
 //    page.on('request', request => { console.log("REQ: " + request.url()); });
 
-    try {
-        await page.goto(url, {waitUntil: 'networkidle0'});
-    } finally {
-        page.removeListener('requestfailed', failedListener);
-        page.removeListener('requestfinished', requestFinishedListener);
-        page.removeListener('response', responseListener);
-    }
-    const hrefs = await page.evaluate(() => {
-        const anchors = document.querySelectorAll('a');
-        return [].map.call(anchors, a => a.href);
-    });
+  try {
+    await page.goto(crawlUrl, {waitUntil: 'networkidle0'});
+    await waitUntilEmpty(openRequestUrls);
+    await scrollToEnd(page);
+    await waitUntilEmpty(openRequestUrls);
+  } finally {
+    page.removeListener('request', requestListener);
+    page.removeListener('requestfailed', requestFailedListener);
+    page.removeListener('requestfinished', requestFinishedListener);
+    page.removeListener('response', responseListener);
+  }
+  const hrefs = await page.evaluate(() => {
+    const anchors = document.querySelectorAll('a');
+    return [].map.call(anchors, a => a.href);
+  });
 //    await page.screenshot({path: 'example.png'});
 
-    let ret = {url: url};
-    if (succeeded.length > 0) {
-        ret.succeeded = succeeded;
-    }
-    if (failed.length > 0) {
-        ret.failed = failed;
-    }
-    if (hrefs.length > 0) {
-        ret.hrefs = hrefs;
-    }
-    return ret;
+  let ret = {url: crawlUrl};
+  if (succeeded.length > 0) {
+    ret.succeeded = succeeded;
+  }
+  if (failed.length > 0) {
+    ret.failed = failed;
+  }
+  if (hrefs.length > 0) {
+    ret.hrefs = hrefs;
+  }
+  return ret;
 }
 
 function urlToPrefix(url) {
-    let s = url.protocol + "//";
-    if (url.auth) {
-        s = s + url.auth + "@";
-    }
-    return s + url.host;
+  let s = url.protocol + "//";
+  if (url.auth) {
+    s = s + url.auth + "@";
+  }
+  return s + url.host;
 }
 
-function updateTodo(todo, results, checked, currentUrl, hrefs, root) {
-    let rootUrl = new URL(root);
-    const rootUrlStart = urlToPrefix(rootUrl);
+function updateTodo(state, currentUrl, hrefs, root) {
+  const rootUrl = new URL(root);
+  const rootUrlStart = urlToPrefix(rootUrl);
 
-    for (let href of hrefs) {
-        let url = new URL(href, currentUrl);
-        let urlString = url.toString();
-        if (!checked.hasOwnProperty(urlString) && !todo.includes(urlString)) {
-            let urlStart = urlToPrefix(url);
-            if (rootUrlStart.valueOf() === urlStart.valueOf()) {
-                todo.push(urlString)
-            }
-        }
+  for (const href of hrefs) {
+    const url = new URL(href, currentUrl);
+    const urlString = url.toString();
+    const urlStart = urlToPrefix(url);
+    if (!state.checked.hasOwnProperty(urlString) && !state.todo.includes(urlString)) {
+      if (rootUrlStart.valueOf() === urlStart.valueOf()) {
+        state.todo.push(urlString)
+      }
     }
+  }
 }
 
-async function crawlUrls(page, root) {
-    const todo = [root];
-    const checked = {};
-    const results = [];
-    do {
-        const url = todo.pop();
-        let urlResults = await crawlUrl(page, url);
-        results.push(urlResults);
-        if (urlResults.hrefs) {
-            updateTodo(todo, results, checked, url, urlResults.hrefs, root)
+function collectErrors(result) {
+  const ret = {}
+  for (const pageResult of result) {
+    for (const failed of pageResult.failed || []) {
+      const failedUrl = failed.url
+      if (!ret.failedUrl) {
+        ret[failedUrl] = {}
+      }
+      if (pageResult.url !== failedUrl) {
+        if (!ret[failedUrl].loadedBy) {
+          ret[failedUrl].loadedBy = []
         }
-    } while (todo.length !== 0);
-    return results;
+        ret[failedUrl].loadedBy.push(pageResult.url)
+      }
+    }
+  }
+  for (const pageResult of result) {
+    for (const href of pageResult.hrefs || []) {
+      if (ret.hasOwnProperty(href)) {
+        if (!ret[href].linkedBy) {
+          ret[href].linkedBy = []
+        }
+        ret[href].linkedBy.push(pageResult.url)
+      }
+    }
+  }
+  return ret;
+}
+
+function uniqueErrors(state) {
+  return Object.keys(collectErrors(state)).length
+}
+
+function removeFromArray(arr, obj) {
+  arr.splice(arr.indexOf(obj), 1);
+}
+
+async function crawlUrls(state, page, root) {
+  do {
+    const url = state.todo.shift();
+    info("check", url, "checked", Object.keys(state.checked).length, "todo", state.todo.length, "unique errors", uniqueErrors(state.results))
+    state.processing.push(url)
+    const urlResults = await crawlUrl(page, url);
+    removeFromArray(state.processing, url);
+    state.results.push(urlResults);
+    state.checked[url] = true;
+    if (urlResults.hrefs) {
+      updateTodo(state, url, urlResults.hrefs, root)
+    }
+    info("errors", pretty(collectErrors(state.results)))
+  } while (state.todo.length !== 0);
+  info("checked", Object.keys(state.checked).length, "unique errors", uniqueErrors(state.results))
+  info("errors", collectErrors(state.results))
+  info("results", state.results)
+  return state.results;
+}
+
+function pretty(obj) {
+  return JSON.stringify(obj, null, 2)
 }
 
 async function crawl(url) {
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
+  return crawler().crawl(url)
+}
 
-    try {
-        return await crawlUrls(page, url);
-    } finally {
+function crawler() {
+  let browser, page;
+  return {
+    todo: [],
+    results: [],
+    checked: {},
+    processing: [],
+    crawl: async function (root) {
+      if (!browser) {
+        browser = await puppeteer.launch({headless: true});
+      }
+      if (!page) {
+        page = await browser.newPage();
+      }
+
+      this.todo.push(root);
+      try {
+        return await crawlUrls(this, page, root);
+      } finally {
         await browser.close();
+      }
     }
+  };
 }
 
 if (module) {
-    module.exports = {
-        crawl: crawl
-    }
+  module.exports = {
+    crawl: crawl,
+    crawler: crawler
+  }
 }
--- test/test.js
+++ test/test.js
@@ -12,26 +12,33 @@ function eq(was, expected) {
 }
 */
 
-describe('Test', () => {
-    it('Should crawl linked pages', async () => {
-            app.setPageData({
-                a: {
-                    hrefs: ["b", "c"]
-                },
-                b: {}
-            });
-            const res = await checkSite.crawl(app.makeUrl("a"));
-            assert.deepStrictEqual(res, [{
-                "url": app.makeUrl("a"),
-                "succeeded": [app.makeUrl("a")],
-                "hrefs": [app.makeUrl("b"), app.makeUrl("c")]
-            }, {
-                "url": app.makeUrl("c"),
-                "failed": [app.makeUrl("c")]
-            }, {
-                "url": app.makeUrl("b"),
-                "succeeded": [app.makeUrl("b")]
-            }])
-        }
-    )
-});
+describe('Three pages, two links, one page that does not exist', () => {
+  const pages = {
+    a: {
+      hrefs: ["b", "c"]
+    },
+    b: {}
+  };
+  const expectedResult = [{
+    "url": app.makeUrl("a"),
+    "succeeded": [app.makeUrl("a")],
+    "hrefs": [app.makeUrl("b"), app.makeUrl("c")]
+  }, {
+    "url": app.makeUrl("b"),
+    "succeeded": [app.makeUrl("b")]
+  }, {
+    "url": app.makeUrl("c"),
+    "failed": [
+      {
+        "status": 404,
+        "url": app.makeUrl("c")
+      }
+    ]
+  }];
+
+  it('Should crawl linked pages', async () => {
+    app.setPageData(pages);
+    const res = await checkSite.crawl(app.makeUrl("a"));
+    assert.deepStrictEqual(res, expectedResult)
+  })
+})
